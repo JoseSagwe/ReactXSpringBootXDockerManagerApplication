6b831812a238dd5faccb96c1b1e6469c829fa753
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-09 00:18:25 +0300

    sexcond commit

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -3,7 +3,7 @@ import './TodoApp.css'
 export default function TodoApp(){
     return(
         <div className="TodoApp">
-            To Management Application
+            Task Management Application
             <LoginComponent></LoginComponent>
             {/* <WelcomeComponent></WelcomeComponent> */}
         </div>
@@ -15,6 +15,9 @@ function LoginComponent(){
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
 
     function handleUsernameChange(event){
         setUsername(event.target.value);
@@ -23,8 +26,43 @@ function LoginComponent(){
         setPassword(event.target.value); 
     }
 
+    function handleSubmit(){
+        if(username==="Developer" && password==="1234"){
+            console.log("success")
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+        } else{
+            console.log("Failed")
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
+    
+// function SuccessMessageComponent(){
+//     if (showSuccessMessage){
+//     return  <div className="successMessage">Authenticated Successfully</div>
+//     } else{
+//         return null
+//     }
+// }
+    
+// function ErrorMessageComponent(){
+//     if (showErrorMessage){
+//     return    <div className="errorMessage">Authentcation Failed. Please Check Your Credentials</div>
+//     } else{
+//         return null
+//     }
+// }
+
+
+
     return(
         <div className="LoginForm">
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage">Authentcation Failed. Please Check Your Credentials</div>}
+            {/* <SuccessMessageComponent></SuccessMessageComponent>
+            <ErrorMessageComponent></ErrorMessageComponent> */}
         <div>
             <label>User Name: </label>
             <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
@@ -34,7 +72,7 @@ function LoginComponent(){
             <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
         </div>
         <div>
-            <button type="button" name="login">login</button>
+            <button type="button" name="login" onClick={handleSubmit}>login</button>
         </div>
     </div>
     )
