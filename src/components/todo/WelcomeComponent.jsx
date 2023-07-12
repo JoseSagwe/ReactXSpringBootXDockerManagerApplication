afac1309a36637b191673d25d342102258d74d3f
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-12 11:56:45 +0300

    fourth

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -3,12 +3,19 @@ export default function WelcomeComponent(){
 
     const {username} = useParams()
 
+    function callHelloRestApi(){
+        console.log("called")
+    }
+
     return(
         <div className="Welcome">
         <div>
             <h1>Welcome {username}</h1>
             <h2>Jsphere Sofware is working hard to produce most impactful sofwares</h2>
             <Link to="/todos">Manage To Do</Link>
+        </div>
+        <div>
+            <button className='btn btn-success m-5' onClick={callHelloRestApi}>Call Hello API</button>
         </div>
         </div>
     )
