e8d300894181750c212b5776c4524a016fbb2594
JoseSagwe <Josephsagwe421@gmail.com>
2023-09-01 22:09:43 +0300

    F

--- src/components/todo/ListTodosComonent.jsx
+++ src/components/todo/ListTodosComonent.jsx
@@ -8,6 +8,9 @@ import { useNavigate } from "react-router-dom"
 
 
 
+
+
+
 function ListTodosComponent() {
     //date
     const today = new Date()
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,13 +10,6 @@ import HeaderComponent from './HearderComponent'
 import AuthProvider, {useAuth} from './security/AuthContext'
 
 
-
-
-
-
-
-
-
 export default function TodoApp(){
     function AuthenticatedRoute({children}) {
         const authContext = useAuth()
